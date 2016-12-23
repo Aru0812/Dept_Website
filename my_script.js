4c73d5c5bec031b5d29d6e0ca5872e868913617b
Aru0812 <aru.gupta0812@gmail.com>
2016-12-22 23:53:53 -0800

    Add files via upload

---
+++ my_script.js
@@ -0,0 +1,15 @@
+$("#sub").click(function()
+{
+$.post($("#myform").attr("action"),$("#myform:input").serializeArray(),function(info){$("#result").html(info);});
+clearInput();
+});
+$("#myform").submit(function(){
+	return false;
+});
+
+function clearInput() {
+	$("myform:input").each function()
+	{
+		$(this).val('');
+	});
+}
